9d75467fb1d288ca659e1ef19727550d26716294
Diogo Ferreira <ddiogof20@gmail.com>
2025-04-26 16:57:21 -0300

    feat(components): criação do componente de button e configuração do tema

---
+++ src/components/button.tsx
@@ -0,0 +1,26 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+	variant: "primary" | "secondary";
+}
+
+const Button = ({
+  children,
+  className = "",
+	variant,
+  ...props
+}: ButtonProps) => {
+	const variants = {
+		primary: "bg-purple-9 dark:bg-purple-dark-9 hover:bg-purple-10 dark:hover:bg-purple-dark-10 text-white rounded-xs font-functional font-normal text-base px-5 py-3 cursor-pointer active:bg-purple-8 dark:active:bg-purple-dark-8 disabled:bg-mauve-9 dark:disabled:bg-mauve-dark-9 disabled:cursor-not-allowed disabled:text-mauve-10 dark:disabled:text-mauve-dark-11 disabled:hover:bg-mauve-9 dark:disabled:hover:bg-mauve-dark-9 shadow-none",
+		secondary: `bg-purple-alpha-2 dark:bg-purple-dark-alpha-2 hover:bg-purple-alpha-3 dark:hover:bg-purple-dark-alpha-3 active:bg-purple-alpha-1 dark:active:bg-purple-dark-alpha-1 disabled:bg-mauve-alpha-3 dark:disabled:bg-mauve-dark-alpha-3 disabled:hover:bg-mauve-alpha-3 dark:disabled:hover:bg-mauve-dark-alpha-3 rounded-xs font-functional font-normal text-base px-5 py-3 cursor-pointer disabled:cursor-not-allowed shadow-none text-purple-alpha-12 dark:text-purple-dark-alpha-12 disabeld:text-mauve-alpha-10 dark:disabled:text-mauve-dark-alpha-10`
+	}
+
+  return (
+    <button 
+      className={`${variants[variant]} ${className}`}
+      {...props}>
+      {children}
+    </button>
+  );
+}
+
+export { Button };
