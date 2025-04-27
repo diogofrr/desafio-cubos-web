b7cf8c192923cce695cd24829e87b619d1ca55cf
Diogo Ferreira <ddiogof20@gmail.com>
2025-04-26 22:09:50 -0300

    feat(assets): salvamento de ícones e criação de componentes para tratar imagens

---
+++ src/components/icon.tsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import Image from 'next/image';
+
+interface IconProps {
+  name: string;
+  className?: string;
+}
+
+const Icon = ({ name, className }: IconProps) => {
+  const iconPath = `/icons/${name}.svg`;
+
+  return (
+    <Image 
+      src={iconPath} 
+      alt={`${name} icon`}
+      className={`${className}`}
+			height={24}
+			width={24}
+    />
+  );
+};
+
+export default Icon;
